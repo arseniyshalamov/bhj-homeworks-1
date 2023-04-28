63eef9f9463fea1d99ffd629a907df65922f4deb
arseniy shalamov <arsenshalamov16@gmail.com>
2023-04-28 16:34:15 +0300

    newHW

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,9 +1,10 @@
 const editor = document.getElementById("editor");
 const clearBtn = document.getElementById("clear_btn");
 
+
 editor.value = localStorage.text;
 editor.oninput = () => localStorage.text = editor.value;
 clearBtn.onclick = () => {
     editor.value = "";
-    localStorage.text = editor.value;
+    localStorage.clear();
 }
