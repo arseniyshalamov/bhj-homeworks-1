44c872b9371d900dd5f627c6f7ea3f98e4f75ebb
arseniy shalamov <arsenshalamov16@gmail.com>
2023-04-29 05:10:03 +0300

    changeHomeW

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,6 +1,6 @@
 const signinForm = document.getElementById("signin__form");
 const btn = document.querySelector(".btn");
-const control = Array.from(document.querySelector(".control"));
+const control = Array.from(document.querySelectorAll(".control"));
 const signin = document.querySelector(".signin");
 const welcome = document.getElementById("welcome");
 const userId = document.getElementById("user_id");
@@ -8,29 +8,22 @@ const userId = document.getElementById("user_id");
 btn.addEventListener('click', elem => {
     elem.preventDefault();
 
-    let formDate = new formDate(signinForm);
+    let formData = new FormData(signinForm);
     let xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-    xhr.send(formDate);
+    xhr.responseType = 'json'; 
+    xhr.send(formData);
 
-    xhr.addEventListener('readystatechange', () => {
-        //описывает состояние загрузки и сравнивает xhr.readyState === xhr.DONE
-        if (xhr.readyState === xhr.DONE) {
+    xhr.addEventListener('load', () => {
+        if (xhr.status === 200) {
             localStorage.setItem('id', control[0].value);
             localStorage.setItem('password', control[1].value);
-            localStorage.getItem('id');
-            let response = JSON.parse(xhr.response);
-
-            if (!response.success) {
-                alert("Неверный логин/пароль");
-                localStorage.clear();
-                control[0].value = "";
-                control[1].value = "";
-            } else {
-                signin.classList.remove("signin_active");
-                welcome.classList.add("welcome_active");
-                userId.textContent = localStorage.getItem("id");
-            }
+            userId.textContent = localStorage.getItem("id");
+            signin.classList.remove("signin_active");
+            welcome.classList.add("welcome_active");
+        } else {
+            alert("Неверный логин/пароль");
+            signinForm.reset();
         }
     })
 })
--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,9 +2,9 @@ const editor = document.getElementById("editor");
 const clearBtn = document.getElementById("clear_btn");
 
 
-editor.value = localStorage.text;
-editor.oninput = () => localStorage.text = editor.value;
+editor.value = localStorage.getItem("text");
+editor.oninput = () => localStorage.setItem("text", editor.value);
 clearBtn.onclick = () => {
     editor.value = "";
-    localStorage.clear();
+    localStorage.removeItem("text");
 }
